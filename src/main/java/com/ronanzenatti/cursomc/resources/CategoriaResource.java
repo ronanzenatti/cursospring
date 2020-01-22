b3bdfe6dab031b61464eaa51d194170086a207e2
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2020-01-21 22:36:25 -0300

    Criação da Categoria Domain

--- src/main/java/com/ronanzenatti/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ronanzenatti/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.ronanzenatti.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ronanzenatti.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest Está funcionando!!!";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informatática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 }
