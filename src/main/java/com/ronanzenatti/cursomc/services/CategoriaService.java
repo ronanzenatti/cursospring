4dbeeb01880306060b1d78a8e9bbc2eeef3205ee
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2020-01-22 15:32:25 -0300

    Erro Personalizado Categoria

--- src/main/java/com/ronanzenatti/cursomc/services/CategoriaService.java
+++ src/main/java/com/ronanzenatti/cursomc/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.ronanzenatti.cursomc.domain.Categoria;
 import com.ronanzenatti.cursomc.repositories.CategoriaRepository;
+import com.ronanzenatti.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,8 @@ public class CategoriaService {
 
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public List<Categoria> findAll() {
